6133cff86cc95ddd421b2402d556e7ac0e8cbc25
Dorstol <olehsofronov@gmail.com>
2024-10-16 13:51:58 +0300

    implement categorys for questions

--- quiz/models.py
+++ quiz/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Question(models.Model):
     text = models.CharField(max_length=255)
-    
-    
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, related_name='questions')
+
     def __str__(self):
         return self.text
 
@@ -16,3 +16,10 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.text
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
